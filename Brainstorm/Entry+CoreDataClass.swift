b294b076387bf8c32bbc87b69dee84e8e00c4b2a
jbond21 <67006940+jbond21@users.noreply.github.com>
2020-12-03 13:38:09 -0500

    first commit

---
+++ Brainstorm/Entry+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entry+CoreDataClass.swift
+//  Brainstorm
+//
+//  Created by Jordan Bond on 11/30/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Entry)
+public class Entry: NSManagedObject {
+
+}
